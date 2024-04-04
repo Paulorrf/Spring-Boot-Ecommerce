1f8f2fad6ffee176154013cecae6ffebf7cce82c
Paulo <paulorenatorf@hotmail.com>
2024-04-03 23:48:21 -0300

    added route to check jwt

--- src/main/java/com/ecommerce/controllers/HomeController.java
+++ src/main/java/com/ecommerce/controllers/HomeController.java
@@ -5,7 +5,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,4 +40,25 @@ public class HomeController {
         //System.out.println(authentication.getDetails().toString());
         return "protected";
     }
+
+    @GetMapping("/cookie")
+	String cookie(@AuthenticationPrincipal OidcUser principal) {
+		return String.format("""
+					<h1>Oauth2 🔐  </h1>
+				<h3>Principal: %s</h3>
+				<h3>Email attribute: %s</h3>
+				<h3>Authorities: %s</h3>
+				<h3>JWT: %s</h3>
+				""", principal, principal.getAttribute("email"), principal.getAuthorities(),
+				principal.getIdToken().getTokenValue());
+	}
+
+	@GetMapping("/jwt")
+	String jwt(@AuthenticationPrincipal Jwt jwt) {
+		return String.format("""
+				Principal: %s\n
+				Email attribute: %s\n
+				JWT: %s\n
+				""", jwt.getClaims(), jwt.getClaim("email"), jwt.getTokenValue());
+	}
 }

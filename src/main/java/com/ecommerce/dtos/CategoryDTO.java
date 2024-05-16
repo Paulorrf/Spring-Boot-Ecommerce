71fa246d181ee7862c048f01954e727fedb3b9a3
Paulo <paulorenatorf@hotmail.com>
2024-05-16 14:13:33 -0300

    not using lombok

--- src/main/java/com/ecommerce/dtos/CategoryDTO.java
+++ src/main/java/com/ecommerce/dtos/CategoryDTO.java
@@ -7,15 +7,44 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/*
+
 @Data
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+*/
 public class CategoryDTO {
 
     private String name;
 
     private String description;
+
+    public CategoryDTO() {
+    }
+
+    public CategoryDTO(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
 
 }
